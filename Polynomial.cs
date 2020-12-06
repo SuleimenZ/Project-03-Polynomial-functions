d760b2857869d5350ab88f4e13ad6eadcc5b9dad
SuleimenZ <62009948+SuleimenZ@users.noreply.github.com>
2020-12-06 23:49:17 +0100

    Add project files.

---
+++ Polynomial.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_03_Polynomial_functions
+{
+    class Polynomial
+    {
+        List<Point2D> PointList = new List<Point2D>();
+        List<double> Parameters = new List<double>();
+        List<bool> ParameterSigns = new List<bool>();
+        List<double> DerivativeParameters = new List<double>();
+        List<bool> DerivativeSigns = new List<bool>();
+
+        public Polynomial(List<Point2D> pointList)
+        {
+            PointList = pointList;
+            Matrix matrix = new Matrix(pointList);
+            matrix.Solve();
+            Parameters = matrix.ReturnAnswers();
+            foreach (double parameter in Parameters)
+            {
+                if (parameter >= 0)
+                {
+                    ParameterSigns.Add(true);
+                }
+                else
+                {
+                    ParameterSigns.Add(false);
+                }
+            }
+        }
+
+        public void ShowFunction()
+        {
+            string function = "f(x) = ";
+            string sign = ParameterSigns.First() ? "" : "-";
+
+            function += $"{sign}{Math.Round(Parameters.First(), 5).ToString().Replace("-", "")}x^{Parameters.Count()-1} ";
+
+            for (int i = 1; i < Parameters.Count() - 1; i++)
+            {
+                sign = ParameterSigns[i] ? "+" : "-";
+                function += $"{sign} {Math.Round(Parameters[i], 5).ToString().Replace("-", "")}x^{Parameters.Count()-1 - i} ";
+            }
+
+            sign = ParameterSigns.Last() ? "+" : "-";
+            function += $"{sign} {Math.Round(Parameters.Last(), 5).ToString().Replace("-", "")}";
+
+            Console.WriteLine(function);
+        }
+
+        public double Solve(double x)
+        {
+            double answer = Parameters.Last();
+
+            for (int i = 0; i < Parameters.Count() - 1; i++)
+            {
+                answer += Parameters[i]*Math.Pow(x, Parameters.Count()-1 - i);
+            }
+
+            return answer;
+        }
+
+        public void ShowSolution(double x)
+        {
+            Console.WriteLine($"f({x}) = {Solve(x)}");
+        }
+
+        public void SolveDerivatives()
+        {
+            for (int i = 0; i < Parameters.Count - 1; i++)
+            {
+                DerivativeParameters.Add((Parameters.Count-1-i) * Parameters[i]);
+                DerivativeSigns.Add(DerivativeParameters[i] >= 0 ? true : false);
+            }
+        }
+
+        public void ShowDerivative()
+        {
+            SolveDerivatives();
+            Console.WriteLine("Derivative:\n");
+            string derivative = "f'(x) = ";
+            string sign = DerivativeSigns.First() ? "" : "-";
+
+            derivative += $"{sign}{Math.Round(DerivativeParameters.First(), 5).ToString().Replace("-", "")}x^{DerivativeParameters.Count() - 1} ";
+
+            for (int i = 1; i < DerivativeParameters.Count() - 1; i++)
+            {
+                sign = DerivativeSigns[i] ? "+" : "-";
+                derivative += $"{sign} {Math.Round(DerivativeParameters[i], 5).ToString().Replace("-", "")}x^{DerivativeParameters.Count() - 1 - i} ";
+            }
+
+            sign = DerivativeSigns.Last() ? "" : "-";
+            derivative += $"{sign} {Math.Round(DerivativeParameters.Last(), 5).ToString().Replace("-", "")}";
+
+            Console.WriteLine(derivative);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_03_Polynomial_functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int id = 1;
+            bool exitIfFalse = true;
+            string userInput;
+            List<Point2D> pointList = new List<Point2D>();
+
+            Console.WriteLine("Please input points in x y representation\nType END to finish");
+
+            while (exitIfFalse)
+            {
+                Console.Write($"Point {id++}: ");
+                userInput = Console.ReadLine();
+                if (userInput == "END")
+                {
+                    break;
+                }
+                pointList.Add(new Point2D(userInput));
+            }
+            Polynomial polynom = new Polynomial(pointList);
+            
+            Console.WriteLine($"Resulting polynomial will be of the order {pointList.Count - 1}");
+            polynom.ShowFunction();
+            polynom.ShowSolution(-1);
+            polynom.ShowSolution(0);
+            polynom.ShowSolution(1);
+            polynom.ShowDerivative();
+            Console.ReadLine();
+        }
+    }
+}
